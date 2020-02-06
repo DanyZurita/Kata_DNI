a391e257e15917febc3f6ae23a600450eeb1e8bf
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-02-06 00:54:10 +0000

    test(DNI3,4,5,6,7): Aadir 5 nuevos casos test

--- src/test/java/edu/elsmancs/dni/domain/AsignacionTest.java
+++ src/test/java/edu/elsmancs/dni/domain/AsignacionTest.java
@@ -25,9 +25,9 @@ public class AsignacionTest {
         dni.setDNI(dniCompleto);
         assertEquals(dni.getDNI(), "43474645E");
         
-        System.out.println("- Test getDni:");
+        System.out.println("- Test DNI1 getDni:");
         System.out.println(dni.getDni());
-        System.out.println("- Test getDNI:");
+        System.out.println("- Test DNI1 getDNI:");
         System.out.println(dni.getDNI());
         System.out.println('\n');
     }
@@ -40,9 +40,84 @@ public class AsignacionTest {
         dni.setDNI(dniCompleto);
         assertEquals(dni.getDNI(), "43474644K");
         
-        System.out.println("- Test getDni:");
+        System.out.println("- Test DNI2 getDni:");
         System.out.println(dni.getDni());
-        System.out.println("- Test getDNI:");
+        System.out.println("- Test DNI2 getDNI:");
+        System.out.println(dni.getDNI());
+        System.out.println('\n');
+    }
+    
+    @Test
+    public void DNI3() {
+        Asignacion dni = new Asignacion("85093596");
+        char letra = dni.calcularLetra(dni.getDni());
+        String dniCompleto = dni.añadirLetra(letra);
+        dni.setDNI(dniCompleto);
+        assertEquals(dni.getDNI(), "85093596J");
+        
+        System.out.println("- Test DNI3 getDni:");
+        System.out.println(dni.getDni());
+        System.out.println("- Test DNI3 getDNI:");
+        System.out.println(dni.getDNI());
+        System.out.println('\n');
+    }
+    
+    @Test
+    public void DNI4() {
+        Asignacion dni = new Asignacion("04632735");
+        char letra = dni.calcularLetra(dni.getDni());
+        String dniCompleto = dni.añadirLetra(letra);
+        dni.setDNI(dniCompleto);
+        assertEquals(dni.getDNI(), "04632735Y");
+        
+        System.out.println("- Test DNI4 getDni:");
+        System.out.println(dni.getDni());
+        System.out.println("- Test DNI4 getDNI:");
+        System.out.println(dni.getDNI());
+        System.out.println('\n');
+    }
+    
+    @Test
+    public void DNI5() {
+        Asignacion dni = new Asignacion("81198036");
+        char letra = dni.calcularLetra(dni.getDni());
+        String dniCompleto = dni.añadirLetra(letra);
+        dni.setDNI(dniCompleto);
+        assertEquals(dni.getDNI(), "81198036D");
+        
+        System.out.println("- Test DNI5 getDni:");
+        System.out.println(dni.getDni());
+        System.out.println("- Test DNI5 getDNI:");
+        System.out.println(dni.getDNI());
+        System.out.println('\n');
+    }
+    
+    @Test
+    public void DNI6() {
+        Asignacion dni = new Asignacion("12054248");
+        char letra = dni.calcularLetra(dni.getDni());
+        String dniCompleto = dni.añadirLetra(letra);
+        dni.setDNI(dniCompleto);
+        assertEquals(dni.getDNI(), "12054248V");
+        
+        System.out.println("- Test DNI6 getDni:");
+        System.out.println(dni.getDni());
+        System.out.println("- Test DNI6 getDNI:");
+        System.out.println(dni.getDNI());
+        System.out.println('\n');
+    }
+    
+    @Test
+    public void DNI7() {
+        Asignacion dni = new Asignacion("90567867");
+        char letra = dni.calcularLetra(dni.getDni());
+        String dniCompleto = dni.añadirLetra(letra);
+        dni.setDNI(dniCompleto);
+        assertEquals(dni.getDNI(), "90567867P");
+        
+        System.out.println("- Test DNI7 getDni:");
+        System.out.println(dni.getDni());
+        System.out.println("- Test DNI7 getDNI:");
         System.out.println(dni.getDNI());
         System.out.println('\n');
     }
